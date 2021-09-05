6618df8854757dd8485db11571df7d0d68563e77
Daniel Guerrero <76658403+dguerreroq@users.noreply.github.com>
2021-09-05 17:10:38 -0500

    src

---
+++ src/routes/products.routes.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+const router = Router()
+
+import * as productsCtrl from '../controllers/products.controller'
+
+router.get("/", productsCtrl.getProducts);
+
+router.get("/:productId", productsCtrl.getProductById);
+
+router.post(
+  "/",
+  [authJwt.verifyToken, authJwt.isModerator],
+  productsCtrl.createProduct
+);
+
+router.put(
+  "/:productId",
+  [authJwt.verifyToken, authJwt.isModerator],
+  productsCtrl.updateProductById
+);
+
+router.delete(
+  "/:productId",
+  [authJwt.verifyToken, authJwt.isAdmin],
+  productsCtrl.deleteProductById
+);
+
+export default router;
